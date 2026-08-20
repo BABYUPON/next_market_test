ddc5bbf7fe36fb5660524dd021feb8bf0515951c
fuupon <yusimamipu@gmail.com>
2026-08-20 16:27:38 +0900

    params非同期化対応1

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,12 +1,29 @@
-const getSingleItem = (id) => {
-    console.log(id)
-    //const response = fetch("")
+import Image from "next/image"
+
+const getSingleItem = async(id) => {
+    //console.log(id)
+    const response = await fetch('http://localhost:3000/api/item/readsingle/${id}')
+    const jsonData = await response.json()
+    console.log(jsonData)
 }
-const ReadSingleItem = (context) => {
+const ReadSingleItem = async(context) => {
     //console.log(context)
-    getSingleItem(context.params.id)
+    const { id } = await context.params;
+    const singleItem = await getSingleItem(id)
+    console.log(singleItem)
     return (
-        <h1>個別アイテムページだ</h1>
+        <div>
+            <div>
+                <h1 className="h1-style">個別アイテムページだ</h1>
+                <Image src={singleItem.image} width={750} height={500} alt="item-image" priority/>
+            </div>
+            <div>
+                <h1>{singleItem.title}</h1>
+                <h2>{singleItem.price}</h2>
+                <hr/>
+                <p>{singleItem.description}</p>
+            </div>
+        </div>
     )
 }
 
