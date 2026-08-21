01ad76141293ade0d25e176c9854aa56af666491
fuupon <yusimamipu@gmail.com>
2026-08-21 11:16:33 +0900

    いけるかな

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -19,10 +19,10 @@ const ReadSingleItem = async(context) => {
                 <Image src={singleItem?.image} width={750} height={500} alt="item-image" priority/>
             </div>
             <div>
-                <h1>{singleItem.title}</h1>
-                <h2>{singleItem.price}</h2>
+                <h1>{singleItem?.title}</h1>
+                <h2>{singleItem?.price}</h2>
                 <hr/>
-                <p>{singleItem.description}</p>
+                <p>{singleItem?.description}</p>
             </div>
         </div>
     )
