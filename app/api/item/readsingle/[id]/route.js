8ff2bd0fc046612dcaa70ee3f7fabc2c22afec15
fuupon <yusimamipu@gmail.com>
2026-08-21 10:16:04 +0900

    レスポンスのsingleItem参照不可

--- app/api/item/readsingle/[id]/route.js
+++ app/api/item/readsingle/[id]/route.js
@@ -7,7 +7,8 @@ export async function GET(request,context){
      try{
         //await connectDB()
         //const singleItem = await ItemModel.findById(context.params.id)
-        const singleItem = [{"_id":"65478ff981349fcd3162bf87","title":"色えんぴつ","image":"/img2.jpg","price":"1500","description":"使いやすい色えんぴつです。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse maximus est tellus, eget porta leo tristique a. Donec hendrerit massa leo, id tempus dolor vulputate et. Pellentesque consectetur dolor placerat euismod pellentesque. Integer scelerisque, augue ac ullamcorper sodales, neque lectus tristique turpis, id luctus lectus lorem eu tortor. In imperdiet semper accumsan. Etiam pellentesque libero et scelerisque vehicula. Nam quis justo mi. Cras erat ex, rhoncus id blandit id, commodo ac leo. In hac habitasse platea dictumst.","email":"kuma@gmail.com","__v":0}]
+        const singleItem = {"_id":"65478ff981349fcd3162bf87","title":"色えんぴつ","image":"/img2.jpg","price":"1500","description":"使いやすい色えんぴつです。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse maximus est tellus, eget porta leo tristique a. Donec hendrerit massa leo, id tempus dolor vulputate et. Pellentesque consectetur dolor placerat euismod pellentesque. Integer scelerisque, augue ac ullamcorper sodales, neque lectus tristique turpis, id luctus lectus lorem eu tortor. In imperdiet semper accumsan. Etiam pellentesque libero et scelerisque vehicula. Nam quis justo mi. Cras erat ex, rhoncus id blandit id, commodo ac leo. In hac habitasse platea dictumst.","email":"kuma@gmail.com","__v":0}
+        console.log(singleItem)
         return NextResponse.json({message:"アイテム読み取り成功（シングル）", singleItem: singleItem})
         //return NextResponse.json({message:"アイテム読み取り成功（シングル）"})
     }catch{
