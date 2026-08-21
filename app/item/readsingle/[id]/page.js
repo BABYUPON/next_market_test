02081751ab57cae6274bc8ddd2cfe99ba3ca3761
fuupon <yusimamipu@gmail.com>
2026-08-21 16:20:40 +0900

    もうそろそろ1

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -10,26 +10,22 @@ const ReadSingleItem = async(context) => {
     //console.log(context)
     const { id } = await context.params;
     console.log(id)
-    const   singleItem = {
-        title: ' ',        
-        image: ' ',
-        price: '0',    
-        description: ' ',
-        email: ' ',
-        __v: 0}
-    singleItem = await getSingleItem(id)
+    const singleItem = await getSingleItem(id)
     console.log(singleItem)
     return (
         <div>
             <div>
                 <h1 className="h1-style">個別アイテムページだ</h1>
-                <Image src={singleItem?.image} width={750} height={500} alt="item-image" priority/>
-            </div>
-            <div>
-                <h1>{singleItem?.title}</h1>
-                <h2>{singleItem?.price}</h2>
-                <hr/>
-                <p>{singleItem?.description}</p>
+                {singleItem.map(item => 
+                    <Link href=""  key={item._id}>
+                        <img src={item.image} width={750} height={500} alt="item-image" priority/>
+                        <div>
+                            <h2>{item.price}</h2>
+                            <h3>{item.title}</h3>
+                            <p>{item.description.substring(0,80)}</p>
+                        </div>
+                    </Link>
+                )}
             </div>
         </div>
     )
