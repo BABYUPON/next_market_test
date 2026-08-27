e7703ee1da6a6d820737b4f9dffcc83e1f18264b
fuupon <yusimamipu@gmail.com>
2026-08-27 13:43:31 +0900

    わかってない

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import { jwtVerify, SignJWT } from "jose"
 
 
-export async function middleware(){
+export async function middleware(request){
     console.log("ミドルウェア")
     const token = await request.headers.get("Authorization")?.split(" ")[1]
     //const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc4NzAzMTQyOH0.BLRWsmfcINRxgb16tJ3od--3nOmC0L7kJlTOUHUeeNw"
