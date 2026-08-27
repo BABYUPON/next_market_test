845e3e536f01025fe051da90c91ca276d1c6b62a
fuupon <yusimamipu@gmail.com>
2026-08-27 10:15:20 +0900

    27日1

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -4,26 +4,28 @@ const getSingleItem = async(id) => {
     //console.log(id)
     const response = await fetch(`http://localhost:3000/api/item/readsingle/${id}`)
     const jsonData = await response.json()
+    const singleItem = jsonData.singleItem
+    return singleItem
     //console.log(jsonData)
 }
 const ReadSingleItem = async(context) => {
     //console.log(context)
     const { id } = await context.params;
     console.log(id)
-    const data = await getSingleItem(id)
-    
-    console.log("dataは", data)
+
+    const singleItem = await getSingleItem(id)
+    console.log("singleItemは", singleItem)
     return (
         <div>
             <div>
                 <h1 className="h1-style">個別アイテムページだ</h1>
-                <Image src={data.image} width={750} height={500} alt="item-image" priority/>
+                <Image src={singleItem.image} width={750} height={500} alt="item-image" priority/>
             </div>
             <div>
-                <h1>{data.title}</h1>
-                <h2>{data.price}</h2>
+                <h1>{singleItem.title}</h1>
+                <h2>{singleItem.price}</h2>
                 <hr/>
-                <p>{data.description}</p>
+                <p>{singleItem.description}</p>
             </div>
         </div>
     )
