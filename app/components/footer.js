48a4ed9c966b99374bc734e576b0e650dd48d109
fuupon <yusimamipu@gmail.com>
2026-08-31 16:34:12 +0900

    おばあちゃん

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <p>@{new Date().gerFullYear()} Next Market</p>
+            <p>@{new Date().getFullYear()} Next Market</p>
         </footer>
     )
 }
